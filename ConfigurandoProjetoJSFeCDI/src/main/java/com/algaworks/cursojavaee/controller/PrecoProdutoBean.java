5a2402bbebfc0282f54fb23f221b86b28174dfe0
Claudio Magalhães <c.viniciusmagalhaes@gmail.com>
2024-11-20 19:22:44 -0300

    adicionando Scoped

--- ConfigurandoProjetoJSFeCDI/src/main/java/com/algaworks/cursojavaee/controller/PrecoProdutoBean.java
+++ ConfigurandoProjetoJSFeCDI/src/main/java/com/algaworks/cursojavaee/controller/PrecoProdutoBean.java
@@ -1,15 +1,28 @@
 package com.algaworks.cursojavaee.controller;
 
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.cursojavaee.service.CalculadoraPreco;
 
 @Named("meuBean")
-public class PrecoProdutoBean {
+@SessionScoped
+public class PrecoProdutoBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Inject
 	private CalculadoraPreco calculadoraPreco;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Init PrecoProdutoBean");
+	}
 	
 	
 	public double getPreco() {
--- ConfigurandoProjetoJSFeCDI/src/main/java/com/algaworks/cursojavaee/service/CalculadoraPreco.java
+++ ConfigurandoProjetoJSFeCDI/src/main/java/com/algaworks/cursojavaee/service/CalculadoraPreco.java
@@ -1,6 +1,18 @@
 package com.algaworks.cursojavaee.service;
 
-public class CalculadoraPreco {
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class CalculadoraPreco implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Init CalculadoraPreco");
+	}
 	
 	public double calcularPreco(int quantidade, double precoUnitario) {
 		return quantidade * precoUnitario;
