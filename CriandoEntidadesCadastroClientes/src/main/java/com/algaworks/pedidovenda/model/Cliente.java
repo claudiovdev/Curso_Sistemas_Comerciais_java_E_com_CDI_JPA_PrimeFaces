8ce7a5443b4b18c0bece9094306da4f780f59af6
Claudio Magalhães <c.viniciusmagalhaes@gmail.com>
2024-12-03 21:34:59 -0300

    criando entidade de cadastro de cliente

---
+++ CriandoEntidadesCadastroClientes/src/main/java/com/algaworks/pedidovenda/model/Cliente.java
@@ -0,0 +1,15 @@
+package com.algaworks.pedidovenda.model;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String nome;
+	private String email;
+	private String documentoReceitaFederal;
+	
+
+}
